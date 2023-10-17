52e39d27dce3f5befff9ec6239d725f32944d44a
carykees98 <trainboy980@gmail.com>
2023-10-17 14:03:20 -0400

    moved config into main and updated run_once

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -7,10 +7,10 @@
 
 #include <dpp/dpp.h>
 
-json CONFIG = nlohmann::json::parse(std::ifstream("config.json"));
-
 int main()
 {
+	json CONFIG = nlohmann::json::parse(std::ifstream("config.json"));
+
 	dpp::cluster bot(CONFIG["token"],
 		dpp::i_message_content | dpp::i_guild_message_reactions | dpp::i_guilds | dpp::i_guild_members);
 
@@ -19,7 +19,7 @@ int main()
 
 	/* Register slash command here in on_ready */
 	bot.on_ready(
-		[&bot](const dpp::ready_t& event)
+		[&bot, &CONFIG](const dpp::ready_t& event)
 		{
 			if (CONFIG["message-id"].is_null())
 			{
@@ -29,7 +29,7 @@ int main()
 
 				bot.message_create(
 					reactionMessage,
-					[&bot](const dpp::confirmation_callback_t& callback)
+					[&bot, &CONFIG](const dpp::confirmation_callback_t& callback)
 					{
 						bot.log(dpp::ll_info, "[INFO] - Sent Message with ID: " + std::to_string(callback.get<dpp::message>().id));
 						CONFIG["message-id"] = uint64_t(callback.get<dpp::message>().id);
@@ -37,16 +37,13 @@ int main()
 					});
 			}
 
-			if (dpp::run_once<struct register_bot_commands>())
-			{
-				bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_watching, "for reactions"));
-			}
+			bot.set_presence(dpp::presence(dpp::presence_status::ps_online, dpp::activity_type::at_watching, "for reactions"));
 
 			bot.log(dpp::ll_info, "Looking for reaction changes on message " + std::to_string(CONFIG["message-id"].get<uint64_t>()));
 		});
 
 	bot.on_message_reaction_add(
-		[&bot](const dpp::message_reaction_add_t& event)
+		[&bot, &CONFIG](const dpp::message_reaction_add_t& event)
 		{
 			if (event.message_id == dpp::snowflake(CONFIG["message-id"].get<uint64_t>()))
 			{
@@ -68,12 +65,12 @@ int main()
 		});
 
 	bot.on_message_reaction_remove(
-		[&bot](const dpp::message_reaction_remove_t& event)
+		[&bot, &CONFIG](const dpp::message_reaction_remove_t& event)
 		{
 			if (event.message_id == dpp::snowflake(CONFIG["message-id"].get<uint64_t>()))
 			{
 				bot.user_get(event.reacting_user_id,
-					[&bot](const dpp::confirmation_callback_t& callback)
+					[&bot, &CONFIG](const dpp::confirmation_callback_t& callback)
 					{
 						dpp::user_identified user = callback.get<dpp::user_identified>();
 
